ea5fbb742d00516629c8204d4b348e54ccc66546
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-06-01 17:15:54 +0200

    ajout des routes qui sont utilisees dans home

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import App from "./App.jsx";
 import Home from "./components/home/Home.jsx";
 import About from "./components/about/About.jsx";
 import Contact from "./components/contact/Contact.jsx";
-
+import Experiences from "./components/experiences/Experiences.jsx";
+import Domaine from "./components/domaine/Domaine.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -21,6 +22,14 @@ const router = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/experiences",
+        element: <Experiences />,
+      },
+      {
+        path: "/domaine",
+        element: <Domaine />,
       },
       {
         path: "/contact",
