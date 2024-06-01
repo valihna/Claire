a442a35b21ceb3e4df6db49c7d55a5e0843e1d5d
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-06-01 22:01:37 +0200

    ajout des experiences pro

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,7 +1,77 @@
+/* eslint-disable react/no-unescaped-entities */
 // import React from "react";
 
 function Experiences() {
-  return <div>Experiences</div>;
+  return (
+    <div>
+      <div className="about">
+        <h2>Mes expériences professionelles</h2>
+      </div>
+      <div className="paragraph">
+        <p>
+          <br /> Nota-Risques-Urba
+          <br /> <br />
+          Responsable commercial
+          <br />
+          janvier 2019 - février 2024 (5 ans 2 mois) <br />
+        </p>
+        <p>
+          MEDIA IMMO
+          <br /> <br />
+          Commercial sédentaire
+          <br />
+          janvier 2018 - février 2024 (6 ans 2 mois)
+          <br />
+          Vente
+          <br />
+          Management
+          <br />
+          Prospection
+          <br />
+          Gestion portefeuille clients <br />
+        </p>
+        <p>
+          Histoire d'Or Europe
+          <br /> <br />
+          CONSEILLERE DE VENTE
+          <br />
+          novembre 2015 - janvier 2018 (2 ans 3 mois) EVRY
+          <br />
+          ACCUEIL DU CLIENT
+          <br />
+          ECOUTER ET CONSEILLER LE CLIENT
+          <br />
+          VENTE DE BIJOUX - montée en gamme et vente additionnel
+          <br />
+          METTRE EN VALEUR LES VITRINES
+          <br />
+          TENIR SON RAYON
+          <br />
+          GÉRER LE SERVICE APRES VENTE
+          <br />
+        </p>
+        <p>
+          GE Healthcare <br /> <br />
+          COORDINATRICE SAV <br />
+          décembre 2012 - septembre 2015 (2 ans 10 mois) <br />
+          Prise d'appels clients B to B <br />
+          Gestion et suivie des affaires <br />
+          Transmission aux techniciens <br />
+          Planning des techniciens <br />
+          Commande de pièces <br />
+          Élaboration de devis <br />
+          Gérer les maintenances préventive <br /> <br />
+        </p>
+        <br /> <h2>Formation</h2>{" "}
+        <p>
+          C3 ALTERNANCE
+          <br />
+          BTS MANAGEMENT DES UNITES COMMERCIALES · (2014 - 2015)
+        </p>
+      </div>
+      <div className="app-about"></div>
+    </div>
+  );
 }
 
 export default Experiences;
