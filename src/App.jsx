312aac77828f024b874b718fc18426df84e2c965
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-06-01 14:27:13 +0200

    suppression de la page vite react et personnalisation de celle ci

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import profile from "./assets/profile.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={profile} className="profile" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Claire Vancleemput</h1>
+      <div className="card"></div>
+      <p className="read-the-docs">Commerciale</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
