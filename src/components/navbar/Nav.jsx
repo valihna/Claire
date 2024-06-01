d397dc1ede2de650f91e06621a0cbccdb4734883
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-06-01 21:04:45 +0200

    creation de la navbar

---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,24 @@
+import { routes } from "../routes";
+import "./Navbar.css";
+
+const Nav = () => {
+  return (
+    <ul className="hidden lg:flex lg:items-center gap-5 text-sm">
+      {routes.map((route, idx) => {
+        const { link, title } = route;
+        return (
+          <li key={idx}>
+            <a
+              href={link}
+              className="flex items-center gap-1 hover:text-neutral-400 transition-all"
+            >
+              {title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Nav;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,72 @@
-// import React from 'react'
+import { useRef, useState } from "react";
+import { useClickAway } from "react-use";
+import { AnimatePresence, motion } from "framer-motion";
+import { Squash as Hamburger } from "hamburger-react";
+import { Link } from "react-router-dom";
+import { routes } from "./routes";
+import "./Navbar.css";
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+  const ref = useRef(null);
+  useClickAway(ref, () => setOpen(false));
+
+  return (
+    <div className="global-nav fixed-nav">
+      <div className="nav">
+        <div ref={ref} className="lghidden">
+          <Hamburger toggled={isOpen} right={10} size={20} toggle={setOpen} />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
+              >
+                <ul className="grid gap-2">
+                  {routes.map((route, idx) => {
+                    const { link, title } = route;
+
+                    return (
+                      <motion.li
+                        key={title}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{
+                          type: "spring",
+                          stiffness: 260,
+                          damping: 20,
+                          delay: 0.1 + idx / 10,
+                        }}
+                        className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
+                      >
+                        <Link
+                          onClick={() => setOpen(false)}
+                          className="flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                          to={link}
+                        >
+                          <span className="flex gap-1 text-lg">{title}</span>
+                        </Link>
+                      </motion.li>
+                    );
+                  })}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+        <div className="slogan">
+          <h2>
+            <Link to="/home" rel="noopener noreferrer">
+              Le succès avec élégance.
+            </Link>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
