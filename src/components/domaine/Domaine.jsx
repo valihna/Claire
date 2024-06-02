ffdf71d7e1cf523c38374c85a39b53f09a260d7b
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-06-02 10:14:42 +0200

    domaine en cours malgre quelques peripeties

--- src/components/domaine/Domaine.jsx
+++ src/components/domaine/Domaine.jsx
@@ -1,5 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
+import fond from "../../assets/fond.png";
+
 function Domaine() {
-  return <div>Domaine</div>;
+  return (
+    <div className="container">
+      <div className="img-container">
+        <img className="img-fond" src={fond} alt="fond" />
+      </div>
+
+      <div className="about">
+        <h3>Quel est mon domaine d'activité ?</h3>
+      </div>
+      <div className="paragraph">
+        <p>
+          Commerce de lutin du père noel <br />
+          <br /> JE DECRIS MON DOMAINE D ACTIVITE JE SUIS UN CANON DE LA NATURE
+          ET JE VAIS BIENTOT ME MARIER MON FILS EST AUSSI UN CANON OLALA ET OUII
+          AUSSI MON CHIEN EST TELLEMENT BEAU!! <br />
+          <br />
+          BLABLA BLABLA <br />
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Domaine;
--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,73 +1,87 @@
 /* eslint-disable react/no-unescaped-entities */
 // import React from "react";
+import "./Experiences.css";
 
 function Experiences() {
   return (
-    <div>
+    <div className="ex-container">
       <div className="about">
-        <h2>Mes expériences professionelles</h2>
+        <h2>Mes expériences professionnelles</h2>
       </div>
       <div className="paragraph">
-        <p>
-          <br /> Nota-Risques-Urba
-          <br /> <br />
-          Responsable commercial
-          <br />
-          janvier 2019 - février 2024 (5 ans 2 mois) <br />
-        </p>
-        <p>
-          MEDIA IMMO
-          <br /> <br />
-          Commercial sédentaire
-          <br />
-          janvier 2018 - février 2024 (6 ans 2 mois)
-          <br />
-          Vente
-          <br />
-          Management
-          <br />
-          Prospection
-          <br />
-          Gestion portefeuille clients <br />
-        </p>
-        <p>
-          Histoire d'Or Europe
-          <br /> <br />
-          CONSEILLERE DE VENTE
-          <br />
-          novembre 2015 - janvier 2018 (2 ans 3 mois) EVRY
-          <br />
-          ACCUEIL DU CLIENT
-          <br />
-          ECOUTER ET CONSEILLER LE CLIENT
-          <br />
-          VENTE DE BIJOUX - montée en gamme et vente additionnel
-          <br />
-          METTRE EN VALEUR LES VITRINES
-          <br />
-          TENIR SON RAYON
-          <br />
-          GÉRER LE SERVICE APRES VENTE
-          <br />
-        </p>
-        <p>
-          GE Healthcare <br /> <br />
-          COORDINATRICE SAV <br />
-          décembre 2012 - septembre 2015 (2 ans 10 mois) <br />
-          Prise d'appels clients B to B <br />
-          Gestion et suivie des affaires <br />
-          Transmission aux techniciens <br />
-          Planning des techniciens <br />
-          Commande de pièces <br />
-          Élaboration de devis <br />
-          Gérer les maintenances préventive <br /> <br />
-        </p>
-        <br /> <h2>Formation</h2>{" "}
-        <p>
-          C3 ALTERNANCE
-          <br />
-          BTS MANAGEMENT DES UNITES COMMERCIALES · (2014 - 2015)
-        </p>
+        <div>
+          {" "}
+          <p>
+            <br /> Nota-Risques-Urba
+            <br /> <br />
+            Responsable commercial
+            <br />
+            janvier 2019 - février 2024 (5 ans 2 mois) <br />
+          </p>
+        </div>{" "}
+        <div>
+          <p>
+            MEDIA IMMO
+            <br /> <br />
+            Commercial sédentaire
+            <br />
+            janvier 2018 - février 2024 (6 ans 2 mois)
+            <br />
+            Vente
+            <br />
+            Management
+            <br />
+            Prospection
+            <br />
+            Gestion portefeuille clients <br />
+          </p>
+        </div>{" "}
+        <div>
+          <p>
+            Histoire d'Or Europe
+            <br /> <br />
+            CONSEILLERE DE VENTE
+            <br />
+            novembre 2015 - janvier 2018 (2 ans 3 mois) EVRY
+            <br />
+            ACCUEIL DU CLIENT
+            <br />
+            ECOUTER ET CONSEILLER LE CLIENT
+            <br />
+            VENTE DE BIJOUX - montée en gamme et vente additionnel
+            <br />
+            METTRE EN VALEUR LES VITRINES
+            <br />
+            TENIR SON RAYON
+            <br />
+            GÉRER LE SERVICE APRES VENTE
+            <br />
+          </p>
+        </div>{" "}
+        <div>
+          <p>
+            GE Healthcare <br /> <br />
+            COORDINATRICE SAV <br />
+            décembre 2012 - septembre 2015 (2 ans 10 mois) <br />
+            Prise d'appels clients B to B <br />
+            Gestion et suivie des affaires <br />
+            Transmission aux techniciens <br />
+            Planning des techniciens <br />
+            Commande de pièces <br />
+            Élaboration de devis <br />
+            Gérer les maintenances préventive <br /> <br />
+          </p>
+        </div>{" "}
+        <div>
+          <br /> <h2>Formation</h2>{" "}
+        </div>{" "}
+        <div>
+          <p>
+            C3 ALTERNANCE
+            <br />
+            BTS MANAGEMENT DES UNITES COMMERCIALES · (2014 - 2015)
+          </p>
+        </div>
       </div>
       <div className="app-about"></div>
     </div>
